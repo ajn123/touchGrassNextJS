fa6f45464a18c8954a3375a8bd76cbff7a429f2b
ajn123 <alexander.norton@frontgatetickets.com>
2025-03-01 20:00:39 -0500

    updated header

--- app/header.tsx
+++ app/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <div className="sticky bg-white rounded-b-lg">
             <Link href="/">
-                <h1 className="text-2xl font-bold ml-4 py-4 text-black">Events DC</h1>
+                <h1 className="text-2xl hover:scale-x-95 transition-all duration-300 font-bold ml-4 py-4 text-black">Events DC</h1>
             </Link>
         </div>
     );
