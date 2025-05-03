e7793fe49a97006a0197e894736e17ac27ac3fbe
ajn123 <alexander.norton@frontgatetickets.com>
2025-05-03 14:03:44 -0400

    fix date header

--- components/dates/dateHeader.tsx
+++ components/dates/dateHeader.tsx
@@ -2,29 +2,31 @@ import { Event } from "@/types/event";
 
 
 export default function DateHeader(event: Event) {
+
+    console.log(event._id);
         return (
-            <div key={event._id } className="text-sm text-gray-500 mb-2 mt-2">
+            <div key={event._id} className="text-sm text-gray-500 mb-2 mt-2">
                 {event.date && new Date(event.date).toLocaleDateString('en-US', {
                     weekday: 'short',
                     month: 'short',
                     day: 'numeric'
                   })}
-                  {event.schedules && event.schedules.map((schedule) => {
+                  {event.schedules && event.schedules.map((schedule, scheduleIndex) => {
                     return (
-                        <>
+                        <div key={`schedule-${scheduleIndex}`}>
                         {
                             schedule.days.length == 7 && (
-                                <span key={"everyday"} className="inline-block px-2 py-1 mr-1 bg-gray-200 text-gray-700 rounded-full text-xs">
+                                <span key="everyday" className="inline-block px-2 py-1 mr-1 bg-gray-200 text-gray-700 rounded-full text-xs">
                                     Everyday
                                 </span>
                             )
                         }
                             {schedule.days.length < 7 && schedule.days.map((day) => (
-                                <span key={day} className="inline-block px-2 py-1 mr-1 bg-gray-200 text-gray-700 rounded-full text-xs">
+                                <span key={`${scheduleIndex}-${day}`} className="inline-block px-2 py-1 mr-1 bg-gray-200 text-gray-700 rounded-full text-xs">
                                     {day}
                                 </span>
                             ))}
-                        </>
+                        </div>
                     )
                   })}
             </div>
